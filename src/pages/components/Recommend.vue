e49c8eab4614124201dab6a7b635bcf0d67e36fa
fanzhenyang <894587483@qq.com>
2018-04-19 19:30:58 +0800

    创建新分支

---
+++ src/pages/components/Recommend.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+      <li class="item">
+        <div class="item-img-wrapper">
+          <img class="item-img" src="" />
+        </div>
+        <div class="item-info">
+          <p class="item-title"></p>
+          <p class="item-desc"></p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.recommend-title
+  line-height: .8rem
+  text-indent: .2rem
+  background: #ccc
+  margin-top: .2rem
+</style>
