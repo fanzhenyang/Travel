deaea50d96f2f1a5e6a80b857820ba7745950236
fanzhenyang <894587483@qq.com>
2018-04-24 16:30:17 +0800

    finish city css

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,19 +2,25 @@
   <div>
     <home-city></home-city>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
 <script>
 import HomeCity from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/list'
+import CityAlphabet from './components/Alphabet'
 export default {
   data () {
     return {}
   },
   components: {
     HomeCity,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlphabet
   }
 }
 </script>
---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.list
+  position: absolute
+  display: flex
+  flex-direction: column
+  justify-content: center
+  top: 1.58rem
+  right: 0
+  bottom: 0
+  width: .4rem
+  .item
+    text-align: center
+    line-height: .4rem
+    color: #00bcd4
+</style>
