3a40ffcb70c8e71a42fc78962a812c3144cb0676
fanzhenyang <894587483@qq.com>
2018-04-24 11:22:28 +0800

    add search to city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <home-city></home-city>
+    <city-search></city-search>
   </div>
 </template>
 
 <script>
 import HomeCity from './components/Header'
+import CitySearch from './components/Search'
 export default {
   data () {
     return {}
   },
   components: {
-    HomeCity
+    HomeCity,
+    CitySearch
   }
 }
 </script>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search">
+    <input type="text" placeholder="输入城市名或拼音" class="search-input"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.search
+  height: .72rem
+  padding: 0 .1rem
+  background: #00bcd4
+  .search-input
+    box-sizing: border-box
+    width: 100%
+    height: .62rem
+    padding: 0 .1rem
+    line-height: .62rem
+    text-align: center
+    border-radius: .06rem
+    color: #666
+</style>
