0e9c41f7d7f88223acd69fcb53d3457e530711e5
fanzhenyang <894587483@qq.com>
2018-04-28 10:09:21 +0800

    header animation

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,21 +1,26 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/banner'
+import DetailHeader from './components/header'
 export default {
   data () {
     return {}
   },
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+.content
+  height: 50rem
 </style>
---
+++ src/pages/detail/components/header.vue
@@ -0,0 +1,85 @@
+<template>
+  <header>
+    <router-link
+      tag="div"
+      to="/"
+      class="header-abs"
+      v-show="showAbs"
+    >
+      <span class="iconfont header-abs-back">&#xe624;</span>
+    </router-link>
+    <div class="header-flxed"
+      v-show="!showAbs"
+      :style="opacityStyle"
+    >
+      <router-link to="/">
+        <div class="iconfont header-flxed-back">&#xe624;</div>
+      </router-link>
+      景点详情
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      if (top > 60) {
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = { opacity }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.header-abs
+  position: absolute
+  left: .2rem
+  top: .2rem
+  width: .8rem
+  height: .8rem
+  line-height: .8rem
+  border-radius: .4rem
+  text-align: center
+  background: rgba(0, 0, 0, .8)
+  .header-abs-back
+    color: #fff
+    font-size: .4rem
+.header-flxed
+  position: fixed
+  top: 0
+  left: 0
+  right: 0
+  height: .86rem
+  line-height: .86rem
+  text-align: center
+  color: #fff
+  background: #00bcd4
+  font-size: .32rem
+  .header-flxed-back
+    position: absolute
+    width: .64rem
+    text-align: center
+    font-size: .4rem
+    top: 0
+    left: 0
+    color: #fff
+</style>
